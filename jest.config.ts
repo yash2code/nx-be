130d0f67bc5b32b19e0263eb81d1bd13ef81b598
Yash Chaudhary <ychaudh2@its.jnj.com>
2023-02-28 01:37:51 +0530

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'treez-be',
+  preset: './jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/treez-be',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+};
